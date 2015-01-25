cec41deca463e2668a43be9a22997997bc50da3e
jirik <jiri.kozel.cz@gmail.com>
2015-01-25 12:28:22 +0100

    upgrade to ol v3.1.1, branch jirik/ol3/kompas

--- tasks/install.js
+++ tasks/install.js
@@ -19,6 +19,9 @@ module.exports = function(grunt) {
       },
       installPip: {
         command: 'python get-pip.py'
+      },
+      createOlExt: {
+        command: 'node bower_components/ol3/tasks/build-ext.js'
       }
     }
   });
@@ -26,43 +29,6 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   
-  grunt.registerTask(
-    'fixOl3',
-    'Fixing OL3 to get it compatible with our version of Closure Compiler',
-    function() {
-      var path = 'bower_components/ol3/src/ol/color/color.js';
-      var cnt = grunt.file.read(path);
-      cnt = cnt.replace(
-          "    goog.asserts.fail(s + ' is not a valid color');",
-          "    throw Error(s + ' is not a valid color');"
-          );
-      grunt.file.write(path, cnt);
-      path = 'bower_components/ol3/src/ol/geom/simplegeometry.js';
-      cnt = grunt.file.read(path);
-      cnt = cnt.replace(
-          "    goog.asserts.fail('unsupported stride: ' + stride);",
-          "    throw Error('unsupported stride: ' + stride);"
-          );
-      cnt = cnt.replace(
-          "    goog.asserts.fail('unsupported layout: ' + layout);",
-          "    throw Error('unsupported layout: ' + layout);"
-          );
-      grunt.file.write(path, cnt);
-      
-      path = 'bower_components/ol3/src/ol/map.js';
-      cnt = grunt.file.read(path);
-      cnt = cnt.replace(
-          "// FIXME recheck layer/map projection compatability when projection changes",
-          "/**\n" +
-          " * @license OpenLayers 3. See http://openlayers.org/\n" +
-          " * License: https://raw.githubusercontent.com/openlayers/ol3/master/LICENSE.md\n" +
-          " * Version: v3.0.0\n" +
-          " */\n"
-          );
-      grunt.file.write(path, cnt);
-    }
-  );
-
   grunt.registerTask(
     'deletePip',
     'Deleting pip download file',
@@ -72,6 +38,6 @@ module.exports = function(grunt) {
   );
 
   grunt.registerTask('installPip', ['curl:pip', 'shell:installPip', 'deletePip']);
-  grunt.registerTask('install', ['installPip', 'shell:installLinter', 'fixOl3']);
+  grunt.registerTask('install', ['installPip', 'shell:installLinter', 'shell:createOlExt']);
 
 };
