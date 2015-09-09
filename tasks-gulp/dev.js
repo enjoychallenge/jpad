ce16116ceb672c5fdb6261f9af3b51b43f6f4ee7
jirik <jiri.kozel.cz@gmail.com>
2015-09-09 22:11:17 +0200

    gulp 'dev' task runs plovr server, ol3dsCfg has plovrCfgs option

--- tasks-gulp/dev.js
+++ tasks-gulp/dev.js
@@ -1,18 +1,33 @@
 'use strict';
+var spawn = require('child_process').spawn;
+require('./../bower_components/closure-library/closure/goog/bootstrap/nodejs');
+goog.require('goog.array');
 
-module.exports = function (gulp, plugins, gulpcfg) {
+module.exports = function (gulp, plugins, ol3dsCfg) {
   
   gulp.task('dev:serve', function (cb) {
+    //start plovr server
+    var args = ['-jar', 'bower_components/plovr/index.jar', 'serve'];
+    goog.array.extend(args, ol3dsCfg.plovrCfgs);
+    var plovr = spawn('java', args);
+    var logData = function (data) {
+      console.log(data.toString());
+    };
+    plovr.stdout.on('data', logData);
+    plovr.stderr.on('data', logData);
+    plovr.on('close', function (code) {
+      console.log('plovr exited with code ' + code);
+    });
 
-    //1. run your script as a server 
+    //run dev server 
     var server = plugins.liveServer(
         './server/server.js',
-        {env: {OL3DS_APPPATH: gulpcfg.appPath}},
+        {env: {OL3DS_APPPATH: ol3dsCfg.appPath}},
         false
     );
     server.start();
  
-    //restart my server 
+    //restart dev server 
     gulp.watch('./server/server.js', server.start.bind(server));
 
     cb();
