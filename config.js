9e9722c991cb8ce85abc719529bf9731209932a9
jirik <jirik@users.noreply.github.com>
2015-09-10 12:03:47 +0200

    ol3dsCfg has fileMappings property

--- config.js
+++ config.js
@@ -4,7 +4,21 @@ var glob = require('glob');
 var ol3ds = {
   appPath: '/path/to/application/',
   plovrCfgs: glob.sync('src/client/**/*.plovr.json'),
-  port: 9000
+  port: 9000,
+  fileMappings: [
+    {
+      src: 'bower_components/ol3/css/',
+      dest: '_lib/ol3/css/'
+    },
+    {
+      src: 'bower_components/ol3/examples/resources/',
+      dest: '_lib/ol3/examples/resources/'
+    },
+    {
+      src: 'src/client/**/*.png',
+      dest: ''
+    }
+  ]
 };
 
 module.exports = ol3ds;
