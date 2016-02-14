61a25ad6880d7a9f30c1660106a521c5b207e6bb
jirik <jiri.kozel.cz@gmail.com>
2016-02-14 18:37:55 +0100

    es6 working example with imported "goog.string"

--- src/client-es6/example/es6/main.js
+++ src/client-es6/example/es6/main.js
@@ -1,6 +1,8 @@
 import msg from './msg';
+import string from 'goog:goog.string';
 
 ///** @type {string} */
 //const msg = 'test';
+const m = string.quote('hi');
 
-console.log('Message:', msg);
+console.log('Message:', msg, m);
